1144270cf436320738a622084d0b901f967a7ffd
Jamshidbek97 <urologiya@internet.ru>
2025-07-29 08:49:09 +0900

    Feat: Configure environmental variables

--- apps/job-board-batch/src/job-board-batch.module.ts
+++ apps/job-board-batch/src/job-board-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { JobBoardBatchController } from './job-board-batch.controller';
 import { JobBoardBatchService } from './job-board-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [JobBoardBatchController],
   providers: [JobBoardBatchService],
 })
--- apps/job-board-batch/src/main.ts
+++ apps/job-board-batch/src/main.ts
@@ -3,6 +3,6 @@ import { JobBoardBatchModule } from './job-board-batch.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(JobBoardBatchModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.PORT_BATCH ?? 3002);
 }
 bootstrap();
